a17a8ab2881e3dfb3571c979fb5c827347bf1ee9
profmanjupriya <mmuralidharanpriya@sdsu.edu>
2025-01-15 23:31:08 -0800

    extra text removed

--- components/landing-page/intro-cards.tsx
+++ components/landing-page/intro-cards.tsx
@@ -14,14 +14,7 @@ export const IntroCards = () => {
         <h1 className="sm:leading-loose font-roboto_condensed tracking-tight text-3xl text-center text-discordPurpleHover mb-5">
           Meet the Team
         </h1>
-        <p className="font-dm_serif_display text-center text-slate-100">
-          My TAs are amazing people who are available to guide you throughout
-          the semester!
-        </p>
-        <p className="font-dm_serif_display text-center text-slate-100 mb-10">
-          You may approach any TA for the course irrespective of the section you
-          are enrolled in.
-        </p>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {people.map((person, index) => (
             <IntroCard key={index} {...person} />
